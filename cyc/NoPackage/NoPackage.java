da1a0f1e5abcc2ee1a422c60a775d809bfb6c09b
chenyuchao <chenyuchao@jd.com>
2016-06-29 17:20:54 +0800

    NoPackage

---
+++ cyc/NoPackage/NoPackage.java
@@ -0,0 +1,8 @@
+package NoPackage;
+public class NoPackage
+{
+	public void play()
+	{
+		System.out.println("cyc test NoPackage");
+	}
+}
---
+++ main.java
@@ -0,0 +1,10 @@
+////import NoPackage;
+import NoPackage.*;
+import java.util.*;
+public class main{
+	public static void main(String [] args)
+	{
+		NoPackage np=new NoPackage();
+		np.play();
+	}
+}
